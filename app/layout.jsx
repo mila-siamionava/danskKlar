be09c32c3db747b20b604646ae3870b3a6eba233
Liudmila Siamionava <lucy.semenova@gmail.com>
2026-08-24 15:08:01 +0200

    rename files .tsx into .jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,16 +1,11 @@
-import type { Metadata } from "next";
 import "@/styles/global.css";
 
-export const metadata: Metadata = {
+export const metadata = {
   title: "Dansk Trainer",
   description: "Danish language practice",
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>{children}</body>
